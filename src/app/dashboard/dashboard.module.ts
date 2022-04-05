7f45bd6fb1836ab3540bb633edb932b9513aab38
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2022-04-05 02:00:14 -0300

    Router

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+
+//Route
+import { DashboardRoutingModule } from './dashBoard-routing-module';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  exports:[
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule
+  ]
+})
+export class DashboardModule { }
